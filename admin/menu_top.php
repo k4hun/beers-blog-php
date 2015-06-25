cc59790be65bb5510e504f4738de730818a3b4ca
Bartek <bart.kaczor@gmail.com>
2015-06-25 19:21:44 +0200

    admin panel menu

---
+++ admin/menu_top.php
@@ -0,0 +1,6 @@
+<ul class='nav nav-tabs'>
+	<li><a href="../breweries">Breweries</a></li>
+	<li><a href="../styles">Styles</a></li>
+	<li><a href="../beers">Beers</a><li>
+	<li><a href="#">Log out</a><li>
+</ul>
